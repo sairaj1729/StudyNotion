28c8c168e5636d7690aeae339963764effd9ae5c
Sairaj Fulsundar <sairajfulsundar111@gmail.com>
2025-07-11 03:06:34 +0530

    fix: for fixing cors error added the deployed frontend url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "https://study-notion-rosy-five.vercel.app"],
         credentials:true,
     })
 )
