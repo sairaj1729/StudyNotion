1c11b83f7f21c3484f0cb55dbbb64347be32980d
Sairaj Fulsundar <sairajfulsundar111@gmail.com>
2025-07-11 02:27:49 +0530

    fix: correct import path for ResetPassword controller

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { resetPasswordToken } = require('../controllers/resetPassword');
+const { resetPasswordToken } = require('../controllers/ResetPassword');
 
 const userSchema = new mongoose.Schema({
     firstName: {
